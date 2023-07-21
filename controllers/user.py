ff2b00878fb502b90912b3a33e4f5f2672395ab2
Diego <dpardoblacutt@gmail.com>
2023-07-21 18:56:54 -0400

    feat: create controllers

---
+++ controllers/user.py
@@ -0,0 +1,10 @@
+from fastapi import APIRouter
+import strawberry
+from strawberry.asgi import GraphQL
+
+from types.user import Queries, Mutations
+
+user = APIRouter()
+schema = strawberry.Schema(Queries, Mutations)
+graphql_app = GraphQL(schema)
+user.add_route("/graphql", graphql_app)
